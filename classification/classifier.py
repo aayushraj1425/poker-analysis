926820c4836f7904b376ce6f271c951b9f7d950b
Aayush Raj Sah <aayushraj1425@gmail.com>
2026-02-15 19:20:17 -0600

    Fix minor bugs in template matching

--- classification/classifier.py
+++ classification/classifier.py
@@ -5,6 +5,7 @@ class CardClassifier:
     """
     Classifies a detected playing card into rank and suit.
     """
+
     @staticmethod
     def extract_corner(warped_card):
         """
@@ -21,7 +22,7 @@ class CardClassifier:
     @staticmethod
     def preprocess_image(image):
         """
-        Preprocess corner for classification.
+        Preprocess image for classification.
         """
         color = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = color
@@ -40,8 +41,11 @@ class CardClassifier:
         # suit = self.classify_suit(processed_corner)
 
     def template_match(self, warped_card):
+        """
+        Classify the detected card using template matching.
+        """
         suits = ['H','D','C', 'S']
-        ranks = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
         corner = self.extract_corner(warped_card)
         processed_corner = self.preprocess_image(corner)
@@ -49,19 +53,20 @@ class CardClassifier:
         card_templates = [rank + suit for suit in suits for rank in ranks]
         for card_template in card_templates:
             path_to_template = f"./templates/{card_template}.png"
-            template = cv2.imread(path_to_template, cv2.IMREAD_GRAYSCALE)
-            template = self.preprocess_image(template)
+            template = cv2.imread(path_to_template)
+            if template is not None:
+                template = self.preprocess_image(template)
 
             w, h = template.shape[::-1]
-            template = template[:int(0.25 * h), :(0.25 * w)]
+            template = template[:int(0.25 * h), :int(0.25 * w)]
             card = processed_corner.copy()
 
             res = cv2.matchTemplate(card, template, method=cv2.TM_CCOEFF_NORMED)
-            match_threshold = 0.7
+            match_threshold = 0.8
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
             if match_threshold <= max_val:
                 top_left = max_loc
                 bottom_right = (top_left[0] + w, top_left[1] + h)
-
                 cv2.rectangle(card, top_left, bottom_right, 255,2)
-                print(f"Matched card: {card_template}")
+                return card_template
+        return None
